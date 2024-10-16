305fddea5e2deb74828f1bd8961768e436bdcd9c
SahaphapM <65160309@go.buu.ac.th>
2024-10-16 21:53:11 +0700

    addButton.vue

---
+++ src/components/AddButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-btn
+    color="primary "
+    icon="add"
+    :to="props.toLink"
+    @click="clickAddFuction"
+    :label="props.label || 'Add New'"
+    style="height: 55px; width: 200px"
+  >
+    <!-- <q-icon name="mdi-plus" />{{ props.label }} -->
+  </q-btn>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  label?: string;
+  toLink?: string;
+  clickAddFuction: () => void;
+}>();
+</script>
