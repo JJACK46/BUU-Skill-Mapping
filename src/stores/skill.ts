8bc5115d854252a8c638bf6aa43b6d60a77ff2ef
Aphidech Mahakoat <jjack46.flowsick@gmail.com>
2025-01-07 22:44:14 +0700

    fix paging skill

--- src/stores/skill.ts
+++ src/stores/skill.ts
@@ -1,8 +1,12 @@
 import { defineStore } from 'pinia';
 import SkillService from 'src/services/skill';
 import { Skill } from 'src/types/skill';
-import { Dialog, Notify } from 'quasar'
-import { calMaxPage, convertToPageParams, defaultPagination } from 'src/utils/pagination';
+import { Dialog, Notify } from 'quasar';
+import {
+  calMaxPage,
+  convertToPageParams,
+  defaultPagination,
+} from 'src/utils/pagination';
 
 type TitleForm =
   | 'New Skill'
@@ -22,25 +26,27 @@ export const useSkillStore = defineStore('skill', {
     parentId: null as number | null,
     totalSkills: 0,
     qNotify: Notify,
-    onlyHaveSubs: true
-
+    onlyHaveSubs: true,
   }),
   getters: {
     getTitleForm: (state) => state.titleForm,
     getParentId: (state) => state.parentId,
-    getMaxPage: (state) => calMaxPage(state.totalSkills, state.pagination!.rowsPerPage),
+    getMaxPage: (state) =>
+      calMaxPage(state.totalSkills, state.pagination!.rowsPerPage),
     getSkills: (state) => {
       if (state.onlyHaveSubs) {
-        return state.skills.filter(skill => skill.children.length > 0)
+        return state.skills.filter((skill) => skill.children.length > 0);
       }
-      return state.skills
-    }
+      return state.skills;
+    },
   },
   actions: {
     async fetchData() {
-      const { data, total } = (await SkillService.getAll(convertToPageParams(this.pagination, this.search)));
-      this.skills = data
-      this.totalSkills = total
+      const { data, total } = await SkillService.getAll(
+        convertToPageParams(this.pagination, this.search)
+      );
+      this.skills = data;
+      this.totalSkills = total;
     },
     async handleSave() {
       if (this.parentId) {
@@ -49,7 +55,7 @@ export const useSkillStore = defineStore('skill', {
           this.qNotify.create({
             type: 'ok',
             message: 'Sub-Skill created successfully',
-          })
+          });
       } else {
         if (this.form.id) {
           const ok = await SkillService.updateSkill(this.form);
@@ -57,46 +63,61 @@ export const useSkillStore = defineStore('skill', {
             this.qNotify.create({
               type: 'ok',
               message: 'Skill updated successfully',
-            })
+            });
         } else {
           const ok = await SkillService.addSkill(this.form as Skill);
           if (ok)
             this.qNotify.create({
               type: 'ok',
               message: 'Skill created successfully',
-            })
+            });
         }
       }
-      this.fetchData()
+      this.fetchData();
       this.dialogForm = false;
-      this.resetForm()
-      this.parentId = null
-
+      this.resetForm();
+      this.parentId = null;
     },
-    async handleRemove({ id, subSkillId }: { id: number, subSkillId?: number }) {
-      this.qDialog.create({
-        title: 'Confirm',
-        message: 'Are you sure you want to delete this item?',
-        cancel: true,
-        persistent: true
-      }).onCancel(() => {
-        return
-      }).onOk(async () => {
-
-        await SkillService.removeSkill(id);
-        if (subSkillId) {
-          await SkillService.removeSubSkill(id, subSkillId);
-        }
-        this.fetchData()
-      })
+    async handleRemove({
+      id,
+      subSkillId,
+    }: {
+      id: number;
+      subSkillId?: number;
+    }) {
+      this.qDialog
+        .create({
+          title: 'Confirm',
+          message: 'Are you sure you want to delete this item?',
+          cancel: true,
+          persistent: true,
+        })
+        .onCancel(() => {
+          return;
+        })
+        .onOk(async () => {
+          await SkillService.removeSkill(id);
+          if (subSkillId) {
+            await SkillService.removeSubSkill(id, subSkillId);
+          }
+          this.fetchData();
+        });
     },
-    async toggleDialog({ form, title, parentId }: { form?: Partial<Skill>, title?: TitleForm, parentId?: number }) {
+    async toggleDialog({
+      form,
+      title,
+      parentId,
+    }: {
+      form?: Partial<Skill>;
+      title?: TitleForm;
+      parentId?: number;
+    }) {
       this.titleForm = title || 'New Skill';
-      this.parentId = parentId || null
+      this.parentId = parentId || null;
       if (form) {
         this.form = form;
       } else {
-        this.resetForm()
+        this.resetForm();
       }
       this.dialogForm = !this.dialogForm;
     },
--- src/utils/pagination.ts
+++ src/utils/pagination.ts
@@ -4,27 +4,34 @@ import { PageParams } from 'src/types/pagination';
 
 // default pagination of Quasar
 export const defaultPagination = {
-    sortBy: 'id',
-    descending: false,
-    page: 1,
-    rowsPerPage: 10,
-    rowsNumber: undefined,
-} as QTableProps['pagination']
+  sortBy: 'id',
+  descending: false,
+  page: 1,
+  rowsPerPage: 10,
+  rowsNumber: undefined,
+} as QTableProps['pagination'];
 
-export const convertToPageParams = (pag: QTableProps['pagination'], search?: string, filterModel?: Partial<FilterModel>) => {
-    return {
-        page: pag?.page || 1,
-        limit: pag?.rowsPerPage || 10,
-        sort: pag?.sortBy || '',
-        order: pag?.descending ? 'DESC' : 'ASC',
-        search: search || '',
-        branchName: filterModel?.branchName || '',
-        curriculumName: filterModel?.curriculumName || '',
-        facultyName: filterModel?.facultyName || '',
-        subjectName: filterModel?.subjectName || '',
-    } as PageParams
-}
+export const convertToPageParams = (
+  pag: QTableProps['pagination'],
+  search?: string,
+  filterModel?: Partial<FilterModel>
+) => {
+  return {
+    page: pag?.page || 1,
+    limit: pag?.rowsPerPage || 10,
+    sort: pag?.sortBy || '',
+    order: pag?.descending ? 'DESC' : 'ASC',
+    search: search || '',
+    branchName: filterModel?.branchName || '',
+    curriculumName: filterModel?.curriculumName || '',
+    facultyName: filterModel?.facultyName || '',
+    subjectName: filterModel?.subjectName || '',
+  } as PageParams;
+};
 
-export const calMaxPage = (totalItems: number | undefined, rowsPerPage: number | undefined) => {
-    return Math.ceil((totalItems || 0) / (rowsPerPage || 10))
-}
+export const calMaxPage = (
+  totalItems: number | undefined,
+  rowsPerPage: number | undefined
+) => {
+  return Math.ceil((totalItems || 0) / (rowsPerPage || 10));
+};
