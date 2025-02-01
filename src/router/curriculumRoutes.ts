e9801264321c287f1222cae386de89c85960f9f4
Aphidech Mahakoat <jjack46.flowsick@gmail.com>
2025-02-02 01:01:00 +0700

    correct path

--- src/router/curriculumRoutes.ts
+++ src/router/curriculumRoutes.ts
@@ -2,42 +2,42 @@ import type { RouteRecordRaw } from 'vue-router';
 
 export const curriculumRoutes: RouteRecordRaw[] = [
   {
-    path: '/curriculums/:id',
+    path: '/curriculums',
     name: 'Curriculum',
     component: () => import('src/layouts/CurriculumLayout.vue'),
     children: [
       {
-        path: '',
+        path: ':id',
         name: 'Curriculum Id',
         component: () => import('src/pages/Curriculum/CurriculumTab.vue'),
       },
       {
-        path: 'coordinators',
+        path: ':id/coordinators',
         name: 'Coordinators of Curriculum',
         component: () => import('src/pages/Curriculum/CoordinatorsTab.vue'),
       },
       {
-        path: 'plos',
+        path: ':id/plos',
         name: 'PLOs',
         component: () => import('src/pages/Curriculum/PloTab.vue'),
       },
       {
-        path: 'skills',
+        path: ':id/skills',
         name: 'Skills of Curriculum',
         component: () => import('src/pages/Curriculum/SkillTab.vue'),
       },
       {
-        path: 'clos',
+        path: ':id/clos',
         name: 'ClOs',
         component: () => import('src/pages/Curriculum/CloTab.vue'),
       },
       {
-        path: 'subjects',
+        path: ':id/subjects',
         name: 'Subjects of Curriculum',
         component: () => import('src/pages/Curriculum/SubjectTab.vue'),
       },
       {
-        path: 'summary',
+        path: ':id/summary',
         name: 'Summary of Curriculum',
         component: () => import('src/pages/Curriculum/SummaryTab.vue'),
       },
