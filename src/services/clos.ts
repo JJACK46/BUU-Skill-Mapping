d17e48cceb1e4012067972432f7bf5f801efda62
aphiwitrr <168804537+aphiwitrr@users.noreply.github.com>
2025-02-04 18:27:31 +0700

    Create Clos Service

---
+++ src/services/clos.ts
@@ -0,0 +1,34 @@
+import type { Clo } from 'src/types/clo';
+import { api } from 'boot/axios';
+import type { PageParams } from 'src/types/pagination';
+import { HttpStatusCode } from 'axios';
+export class ClosService {
+  static path = 'clos';
+  static async getAll(p?: Partial<PageParams>) {
+    const { data } = await api.get(this.path, { params: p });
+    return {
+      data: data[0],
+      total: data[1],
+    };
+  }
+
+  static async getOne(id: string) {
+    const res = await api.get(`${this.path}/${id}`);
+    return res.data;
+  }
+
+  static async createOne(obj: Partial<Clo>) {
+    const res = await api.post(this.path, obj);
+    return res.status === HttpStatusCode.Created;
+  }
+
+  static async updateOne(obj: Partial<Clo>) {
+    const res = await api.patch(`${this.path}/${obj.id}`, obj);
+    return res.status === HttpStatusCode.Ok;
+  }
+
+  static async removeOne(id: string) {
+    const res = await api.delete(`${this.path}/${id}`);
+    return res.status === HttpStatusCode.Ok;
+  }
+}
