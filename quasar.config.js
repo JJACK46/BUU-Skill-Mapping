d4c5506280e2bd8b31fc2a7108a557c05447e785
Aphidech Mahakoat <jjack46.flowsick@gmail.com>
2025-02-10 21:46:09 +0700

    add json view card

--- quasar.config.js
+++ quasar.config.js
@@ -94,7 +94,7 @@ export default configure(function (/* ctx */) {
         dark: false,
       },
 
-      // iconSet: 'material-icons', // Quasar icon set
+      iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
 
       // For special cases outside of where the auto-import strategy can have an impact
