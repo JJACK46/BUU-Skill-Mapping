f8524bc396c7de38c82accd912d1178b31ed27ad
Aphidech Mahakoat <jjack46.flowsick@gmail.com>
2025-02-11 19:00:42 +0700

    update ui clo+plo

--- src/types/clo.ts
+++ src/types/clo.ts
@@ -4,10 +4,11 @@ import type { Skill } from './skill';
 export type Clo = {
   id?: number;
   name: string;
-  description: string;
-  expectedLevel: 1 | 2 | 3 | 4 | 5;
+  thaiDescription: string;
+  engDescription: string;
+  plo?: Plo;
+  skill?: Skill;
+  // expectedLevel: 1 | 2 | 3 | 4 | 5;
   // courseSpec: CourseSpec; prevent circular dependency
-  plos?: Plo;
-  skills?: Skill;
   // skillexpectedlevels: SkillExpectedLevel[]; ถ้า skill สามารถยืมจากคณะอื่่นได้ จำเป็นที่จะต้องกำหนด ตัวกลาง ระหว่าง skill กับ curriculumn เหมือน coursespec
 };
