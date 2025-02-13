e9639e881b8be95242a09497ca8fe947d8df47ae
“Yanagron <armmer.y47@gmail.cpm>
2025-02-13 20:25:35 +0700

    Fix Type Plo

--- src/types/plo.ts
+++ src/types/plo.ts
@@ -1,4 +1,5 @@
 import type { Clo } from './clo';
+import type { Curriculum } from './curriculum';
 
 export type Plo = {
   id: number;
@@ -6,5 +7,7 @@ export type Plo = {
   thaiDescription: string;
   engDescription: string;
   type: string;
+  curriculum?: Curriculum;
+  curriculumId: number;
   clos?: Clo[]; // CLOs
 };
