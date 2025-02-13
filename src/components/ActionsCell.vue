5d5d3453d6a07bfb80603df8160a8f17b3993bf4
Aphidech Mahakoat <jjack46.flowsick@gmail.com>
2025-02-13 18:27:37 +0700

    create actions cell component

---
+++ src/components/ActionsCell.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-td>
+    <q-btn
+      flat
+      dense
+      round
+      color="grey-8"
+      icon="edit"
+      @click="$emit('handleEdit')"
+    />
+    <q-btn
+      flat
+      dense
+      round
+      color="grey-8"
+      icon="delete"
+      class="q-ml-sm"
+      @click="$emit('handleDelete')"
+    />
+  </q-td>
+</template>
+
+<script setup lang="ts">
+defineEmits<{
+  // (e: 'handleView'): void;
+  (e: 'handleEdit'): void;
+  (e: 'handleDelete'): void;
+}>();
+</script>
